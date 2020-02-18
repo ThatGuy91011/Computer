784fe6f78df00dfe8c3f1e0cb73379a9ee58f9b9
KIRK WALLS <KIRWALLS@uat.edu>
2020-02-18 16:18:43 -0700

    Add project files.

---
+++ Keywords.cpp
@@ -0,0 +1,10 @@
+#include "Keywords.h"
+#include <string>
+
+
+std::string things[];
+
+std::string Keywords::inputKW()
+{
+	things[0] = "Help-Displays this help screen";
+}
---
+++ Memory.h
@@ -0,0 +1,8 @@
+#pragma once
+class Memory
+{
+public:
+	int memory_values[];
+private:
+	
+};
